e668a4e745503fb3d5f33a86d4a31d9e9a4bbdf2
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2020-12-18 22:22:15 +0000

    translated forms

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_wtf import Form, RecaptchaField
 from flask_sqlalchemy import SQLAlchemy
-from forms import contact_form, testimonial#imported forms from the "forms" file in the same directory as this file
-from wtforms.validators import DataRequired, Length, Email
+from forms import contact_form, contact_form_ru, testimonial, testimonial_ru#imported forms from the "forms" file in the same directory as this file
 from datetime import datetime#used in testimonial db to sort them by date and time submitted
 import smtplib#for email to work
 from email.message import EmailMessage#for email to work
@@ -54,23 +53,10 @@ class testimonial_database(db.Model):
 def form():
 
     form = contact_form()#make one of the forms in the "forms" file an object to be used below
-    
-    if request.method == "POST" and "translate" in request.form:#checks the method and whether the 
-        #passed value is translate upon clicking a button to translate the web page
-        operation = request.form["operation"]
-        if operation == "russian":#if option's value in the HTML file is "russian"
-            language1 = "russian"
-            return render_template("/Pages/Main page/main_page.html", a=language1, form=form)#"a" variable used for jinja to react 
-            #on the function to choose russian version of HTML
-      
-        elif operation == "english":#if option's value in the HTML file is "english"
-            language2 = "english"
-            return render_template("/Pages/Main page/main_page.html", b=language2, form=form)#"b" variable used for jinja to react 
-            #on the function to choose english version of HTML
 
     if form.validate_on_submit():#will tell me if the form was validated when it was submitted
         flash("Your message has been sent successfully. I'll be in touch shortly.", "success_flash_message")#the message to notify the user of a successfull submition of the form, the second argument is a category to enable different styling for different flash messages
-        new_data = form_database(name=form.name.data, email=form.email.data, message=form.message.data)#grabs user input from the form and prepares it for the database push
+        new_data = form_database(name=form.name.data, email=form.email.data, message=form.message.data, privacy_pol_checkbox=form.privacy_pol_checkbox.data)#grabs user input from the form and prepares it for the database push
      
 
         MY_EMAIL = os.environ.get('MY_EMAIL')#gets my email from the environmental variable
@@ -108,7 +94,50 @@ def form():
     
     return render_template("/Pages/Main page/main_page.html", form=form)#renders the template at first access to the page and displays the form on the page
 
+@app.route('/main_page/ru', methods=["GET", "POST"]) 
 
+def form_ru():
+
+    form = contact_form_ru()#make one of the forms in the "forms" file an object to be used below
+
+    if form.validate_on_submit():#will tell me if the form was validated when it was submitted
+        flash("Ваше сообщение было отправлено успешно. Я скоро отвечу на ваше сообщение.", "success_flash_message")#the message to notify the user of a successfull submition of the form, the second argument is a category to enable different styling for different flash messages
+        new_data = form_database(name=form.name.data, email=form.email.data, message=form.message.data, privacy_pol_checkbox=form.privacy_pol_checkbox.data)#grabs user input from the form and prepares it for the database push
+
+        MY_EMAIL = os.environ.get('MY_EMAIL')#gets my email from the environmental variable
+        MY_EMAIL_PASSWORD = os.environ.get('MY_EMAIL_PASSWORD')#gets the password to my email from the environmental variable
+        
+        message_for_me = EmailMessage()#It is the base class for the email object model. EmailMessage provides 
+        #the core functionality for setting and querying header fields, for accessing message bodies, and for 
+        # creating or modifying structured messages.
+        message_for_me = f"<b>Client name:</b> {form.name.data}<br></br> <b>Email:</b> {form.email.data} <br></br> <b>Message:</b> {form.message.data}"#an email content formatted in "f" string which uses the data a user inputed in the form
+        msg_for_me = MIMEText( message_for_me, "html")#allows to use HTML in the "f" string above
+        msg_for_me['Subject'] = "New form submitted"#email subject
+        msg_for_me['From'] = MY_EMAIL#uses one of my emails to send this email from to my email below
+        msg_for_me['To'] = "svistoplaskino@hotmail.com"
+
+        message_for_customer = EmailMessage()
+        message_for_customer = f"<p>Dear {form.name.data},<br></br> Thank you for expressing your interest in browebgen web services. I will be in touch with you in the next 24 hours.<br></br> Yours sincerely, <br></br> Dmitry </p>"
+        msg_for_customer = MIMEText(message_for_customer, "html")
+        msg_for_customer['Subject'] = "Спасибо от Browebgen"
+        msg_for_customer['From'] = MY_EMAIL
+        msg_for_customer['To'] = form.email.data
+            
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:#An SMTP instance encapsulates an SMTP connection. It has methods that support a full repertoire of SMTP and ESMTP operations.'smtp.gmail.com' is Gmail server (use this because email is sent from a gmail email address) and 465 is corresponding port
+            smtp.login(MY_EMAIL, MY_EMAIL_PASSWORD)#logs in to my gmail using its password
+            smtp.send_message(msg_for_me)#sends an email with the form data to me once a new form has been submitted
+            smtp.send_message(msg_for_customer)#sends an email to the customer adressing them by the name they specified in the form with an HTML formatted email above
+        try:
+            db.session.add(new_data)#adds new data from the form to the database
+            db.session.commit()#comitts this data to the database 
+            return redirect('/main_page/ru')
+        except:
+            return redirect('/main_page/ru')#redirects to the same page if something went wrong
+    
+    elif not form.validate_on_submit() and request.method == "POST":#if the form failed validation after the data was sent by the user then flash the error massage below
+        flash("Вот не задача. Проверте поля в форме.", "error_flash_message")#the message to notify the user of a mistake upon submition of the form, the second argument is a category to enable different styling for different flash messages
+    
+    return render_template("/Pages/Main page/ru/main_page-ru.html", form=form)#renders the template at first access to the page and displays the form on the page
 
 
 @app.route('/blog')#needed for the page to get loaded
@@ -116,14 +145,20 @@ def form():
 def blog():
     return render_template("/Pages/Blog/blog.html")
 
+@app.route('/blog/ru')#needed for the page to get loaded
 
+def blog_ru():
+    return render_template("/Pages/Blog/ru/blog-ru.html")
 
 @app.route('/about_me')#needed for the page to get loaded
 
 def about_me():
     return render_template("/Pages/About me/about_me_page.html")
 
+@app.route('/about_me/ru')#needed for the page to get loaded
 
+def about_me_ru():
+    return render_template("/Pages/About me/ru/about_me_page-ru.html")
 
 @app.route('/my_projects', methods=["GET", "POST"])
 
@@ -131,19 +166,10 @@ def testimonials():
 
 #"form" and "testimonials" need to be defined before both if statements below as both are used for rendering of the page
 #it is important so that the testimonials are visible when either a form is submitted or page translated
-#if also prevents the form valiadion messages to be displayed when the web page gets translated
+#it also prevents the form valiadion messages to be displayed when the web page gets translated
     form = testimonial()
     testimonials = testimonial_database.query.order_by(testimonial_database.date_created)#orders testimonials by 
-    #date before being displayed on the screen
-    if request.method == "POST" and "translate" in request.form:#this "if" statement has to be here
-        operation = request.form["operation"]
-        if operation == "russian":
-            language1 = "russian"
-            return render_template("/Pages/My projects/my_projects.html", a=language1, form=form, testimonials=testimonials)
-    
-        elif operation == "english":
-            language2 = "english"
-            return render_template("/Pages/My projects/my_projects.html", b=language2, form=form, testimonials=testimonials)#displays form AND name and testimonial on the page after rendering
+
     if form.validate_on_submit():#will tell me if the form was walidated when it was submitted
         flash("Thank you for leaving your feedback. It's been added below.", "success_flash_message")
         new_testimonial = testimonial_database(name=form.name.data, testimonial=form.testimonial.data)
@@ -168,12 +194,51 @@ def testimonials():
             return redirect('/my_projects')#redirects to "my_projects" page
         except:#if there is an error it just loads the page again
             return redirect('/my_projects')
-
+            
     elif not form.validate_on_submit() and request.method == "POST":#if the form failed validation after the data was sent by the user then flash the error massage below
         flash("Ooops. Please check the fields below.", "error_flash_message")#the message to notify the user of a mistake upon submition of the form, the second argument is a category to enable different styling for different flash messages
 
     return render_template("/Pages/My projects/my_projects.html", form=form, testimonials=testimonials)#displays form AND name and testimonial on the page after rendering
 
+
+@app.route('/my_projects/ru', methods=["GET", "POST"])
+
+def testimonials_ru():
+
+#"form" and "testimonials" need to be defined before both if statements below as both are used for rendering of the page
+#it is important so that the testimonials are visible when either a form is submitted or page translated
+#it also prevents the form valiadion messages to be displayed when the web page gets translated
+    form = testimonial_ru()
+    testimonials = testimonial_database.query.order_by(testimonial_database.date_created)#orders testimonials by 
+
+    if form.validate_on_submit():#will tell me if the form was walidated when it was submitted
+        flash("Спасибо за отставленый отзыв. Он добавился внизу.", "success_flash_message")
+        new_testimonial = testimonial_database(name=form.name.data, testimonial=form.testimonial.data)
+     
+        MY_EMAIL = os.environ.get('MY_EMAIL')
+        MY_EMAIL_PASSWORD = os.environ.get('MY_EMAIL_PASSWORD')
+        
+        message_for_me = EmailMessage()
+        message_for_me = f"<b>Client name:</b> {form.name.data} <br></br> <b>Testimonial:</b> {form.testimonial.data} "
+        msg_for_me = MIMEText( message_for_me, "html")
+        msg_for_me['Subject'] = "New form submitted"
+        msg_for_me['From'] = MY_EMAIL
+        msg_for_me['To'] = "svistoplaskino@hotmail.com"
+            
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(MY_EMAIL, MY_EMAIL_PASSWORD)
+            smtp.send_message(msg_for_me)
+        try:
+            db.session.add(new_testimonial)
+            db.session.commit()
+            return redirect('/my_projects/ru')#redirects to "my_projects" page
+        except:#if there is an error it just loads the page again
+            return redirect('/my_projects/ru')
+            
+    elif not form.validate_on_submit() and request.method == "POST":#if the form failed validation after the data was sent by the user then flash the error massage below
+        flash("Вот не задача. Проверте поля в форме.", "error_flash_message")#the message to notify the user of a mistake upon submition of the form, the second argument is a category to enable different styling for different flash messages
+
+    return render_template("/Pages/My projects/ru/my_projects-ru.html", form=form, testimonials=testimonials)#displays form AND name and testimonial on the page after rendering
 
 
 @app.route('/blog1')#needed for the page to get loaded
@@ -181,15 +246,17 @@ def testimonials():
 def blog1():
     return render_template("/Pages/Blog/blog1/blog1.html")
 
+@app.route('/blog1/ru')#needed for the page to get loaded
+
+def blog1_ru():
+    return render_template("/Pages/Blog/blog1/ru/blog1-ru.html")
+
 
 @app.route('/')#needed for the page to get loaded
 
 def index():
     return render_template("/index.html")
 
-
-def my_projects_ru():
-    return render_template("/Pages/My projects/my_projects-ru.html")#needed for the page to get loaded
 
 if __name__ == '__main__': #The statement is saying: Is this file being run directly 
 #by python or is it being imported? Python assigns the name "__main__" to the script 
--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField#imported fields that leter get rendered in my HTML files that have imput fields
-from wtforms.validators import DataRequired, Length, Email#imports various validators so that they don't need to be coded from scratch
+from wtforms.validators import DataRequired, Length, Email, ValidationError#imports various validators so that they don't need to be coded from scratch
 from flask_wtf import Form, RecaptchaField, Recaptcha #needed to be able to insert a recaptcha field and validate it
 from wtforms import BooleanField as WTBool
-
+from email_validator import validate_email
 #Defines forms that are use on the website and this file gets imported in browebgen_backend.py to get these forms
+import re#allows to read regular expression for email validation in the russian version of the form
 
 class contact_form(FlaskForm):
 
@@ -14,16 +15,52 @@ class contact_form(FlaskForm):
                                  Length(min=3, max=10)])#sets length for the data input
     email = StringField("Email",
                         validators=[DataRequired(), Email()])#Email() validator check if data imputed follows email standard
-    message = TextAreaField("Message", validators=[DataRequired(), Length(min=20, max=100)])
-
-    submit = SubmitField("Send the message")
+    message = TextAreaField("Message", validators=[DataRequired(), Length(min=20, max=200)])
 
     privacy_pol_checkbox = BooleanField("I Agree to Privacy Policy", validators=[DataRequired()])
+    
+    submit = SubmitField("Send the message")
+        
+
+class contact_form_ru(FlaskForm):#using different form for the russian version of the page to be able to translate the rror messages
+
+    recaptcha = RecaptchaField(validators=[Recaptcha(message="Подтвердите что вы не робот.")])#Displays the message specified instead of a defoult one
+
+    name = StringField("Name")
+    email = StringField("Email")
+    message = TextAreaField("Message")
+    submit = SubmitField("Отправить")
+    privacy_pol_checkbox = BooleanField("I Agree to Privacy Policy")
+
+    def validate_name(self, name):#using custom validator to have the error message in russian when field is empty 
+        if not name.data:
+            self.name.errors += (ValidationError("Это поле обязательно."),)#self.name.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(name.data) < 3:
+             self.name.errors += (ValidationError("В поле должно быть между 3 и 10 букв."),)#self.name.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(name.data) > 10:
+            self.name.errors += (ValidationError("В поле должно быть между 3 и 10 букв."),)#self.name.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+
+    def validate_email(self, email):#using custom validator to have the error message in russian when field is empty 
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        if not email.data:
+            self.email.errors += (ValidationError("Это поле обязательно."),)#self.email.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif not (re.search(regex,email.data)):
+            self.email.errors += (ValidationError("Неверный формат почты."),)
+ 
+    def validate_message(self, message):#using custom validator to have the error message in russian when field is empty 
+        if not message.data:
+            self.message.errors += (ValidationError("Это поле обязательно."),)#self.message.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(message.data) < 20:
+             self.message.errors += (ValidationError("В поле должно быть между 20 и 200 букв."),)#self.message.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(message.data) > 200:
+            self.message.errors += (ValidationError("В поле должно быть между 20 и 200 букв."),)#self.message.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+
+    def validate_privacy_pol_checkbox(self, privacy_pol_checkbox):#using custom validator to have the error message in russian when field is empty 
+        if not privacy_pol_checkbox.data:
+            self.privacy_pol_checkbox.errors += (ValidationError("Отметьте это поле если хотите продолжить."),)#self.message.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
 
 
 class testimonial(FlaskForm):
-
-    recaptcha = RecaptchaField(validators=[Recaptcha(message="Please verify that you're not a robot")])
 
     name = StringField("Name", validators=[DataRequired(),
                                  Length(min=3, max=10)])
@@ -33,3 +70,30 @@ class testimonial(FlaskForm):
 
     submit = SubmitField("Post testimonial")
 
+    recaptcha = RecaptchaField(validators=[Recaptcha(message="Please verify that you're not a robot")])
+
+class testimonial_ru(FlaskForm):
+
+    name = StringField("Name")
+
+    testimonial = StringField("Testimonial")
+
+    submit = SubmitField("Отправить")
+
+    recaptcha = RecaptchaField(validators=[Recaptcha(message="Подтвердите что вы не робот.")])
+
+    def validate_name(self, name):#using custom validator to have the error message in russian when field is empty 
+        if not name.data:
+            self.name.errors += (ValidationError("Это поле обязательно."),)#self.name.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(name.data) < 3:
+             self.name.errors += (ValidationError("В поле должно быть между 3 и 10 букв."),)#self.name.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(name.data) > 10:
+            self.name.errors += (ValidationError("В поле должно быть между 3 и 10 букв."),)#self.name.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+
+    def validate_testimonial(self, testimonial):#using custom validator to have the error message in russian when field is empty 
+        if not testimonial.data:
+            self.testimonial.errors += (ValidationError("Это поле обязательно."),)#self.testimonial.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(testimonial.data) < 6:
+             self.testimonial.errors += (ValidationError("В поле должно быть между 6 и 150 букв."),)#self.testimonial.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
+        elif len(testimonial.data) > 150:
+            self.testimonial.errors += (ValidationError("В поле должно быть между 6 и 150 букв."),)#self.testimonial.errors is tuple of Validation errors. It creates tuple from single ValidationError and adds it to your errors 
